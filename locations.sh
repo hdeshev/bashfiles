a7e17fef07fc2155e235d463f8832c1f17e83cb2
Hristo Deshev <hristo@deshev.com>
2010-09-06 23:45:24 +0300

    Fixed a broken path.

--- locations.sh
+++ locations.sh
@@ -4,4 +4,4 @@ alias pmtw='cd d:/deshev/work/postmark-dev/postmark-delivery/src/Tests/Web.Tests
 alias tmp='cd d:/tmp'
 alias n2='cd d:/deshev/work/n2dev'
 alias work='cd d:/deshev/work'
-alias oss='cd d:/deshev/work/OpenSourcework'
+alias oss='cd d:/deshev/work/OpenSource'
