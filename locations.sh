2e93507d812c7e656d0474872278edc4a6ba8f24
Hristo Deshev <hristo@deshev.com>
2011-01-18 19:47:18 +0200

    fixed location aliases

--- locations.sh
+++ locations.sh
@@ -1,7 +1,4 @@
 # location shortcuts
-alias pm='cd d:/deshev/w/postmark-dev/postmark-delivery'
-alias pmtw='cd d:/deshev/w/postmark-dev/postmark-delivery/src/Tests/Web.Tests'
-alias tmp='cd d:/tmp'
-alias n2='cd d:/deshev/w/n2dev'
-alias work='cd d:/deshev/w'
-alias oss='cd d:/deshev/w/OpenSource'
+alias tmp='cd /data/tmp'
+alias work='cd ~/w'
+alias oss='cd ~/oss'
