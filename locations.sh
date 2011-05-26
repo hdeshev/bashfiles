d1b4c5ec12f4a838174bb045eaaaed10d6c0729d
Hristo Deshev <hristo@deshev.com>
2011-05-26 12:49:12 +0300

    A new location shortcut

--- locations.sh
+++ locations.sh
@@ -1,6 +1,7 @@
 # location shortcuts
 alias goss='cd ~/oss'
 alias gpl='cd ~/w/progstr/progstr-log'
+alias gph='cd ~/w/progstr/progstr-html'
 alias gw='cd ~/w'
 alias gtmp='cd /data/tmp'
 alias gsc='cd ~/w/scala'
