4c65a52a15f2a7484a5199775eb0b1eb5fe715d5
Hristo Deshev <hristo@deshev.com>
2013-08-15 00:06:31 +0300

    Better sourcing of python's virtualenvwrapper script.

--- python.sh
+++ python.sh
@@ -2,6 +2,6 @@ export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
 
 export WORKON_HOME="/opt/virtualenvs"
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+source $(which virtualenvwrapper_lazy.sh)
 
 alias python3='python3.3'
