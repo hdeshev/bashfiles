0a20f8456990a73b6d1e6669d957fc481468226b
Hristo Deshev <hristo@deshev.com>
2013-08-16 11:36:15 +0300

    -s option required for gpg-agent SSH agent support.

--- gpg-agent.sh
+++ gpg-agent.sh
@@ -7,6 +7,6 @@ envfile="${HOME}/.gnupg/gpg-agent.env"
 if test -f "$envfile" && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
     eval "$(cat "$envfile")"
 else
-    eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
+    eval "$(gpg-agent -s --daemon --enable-ssh-support --write-env-file "$envfile")"
 fi
 export GPG_AGENT_INFO  # the env file does not contain the export statement
