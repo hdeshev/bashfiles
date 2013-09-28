cc09f51a63123bb868dc1a4e6ffded7b8989d739
Hristo Deshev <hristo@deshev.com>
2013-09-28 17:15:22 +0300

    Load virtualenvwrapper script Debian style.

--- python.sh
+++ python.sh
@@ -2,6 +2,8 @@ export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
 
 export WORKON_HOME="/opt/virtualenvs"
-source $(which virtualenvwrapper_lazy.sh)
+# source $(which virtualenvwrapper_lazy.sh)
+# Ubuntu/Debian packaging places init in bash completion scripts
+source /etc/bash_completion.d/virtualenvwrapper
 
 alias python3='python3.3'
