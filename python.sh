cb5a7de897e417767dc4219b644c7b9a5c5c1190
Hristo Deshev <hristo@deshev.com>
2014-03-19 11:53:32 +0200

    Ditch virtualenvwrapper. Replace with two simple shell functions.

--- python.sh
+++ python.sh
@@ -1,9 +1,28 @@
 export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
-
-export WORKON_HOME="/opt/virtualenvs"
-# source $(which virtualenvwrapper_lazy.sh)
-# Ubuntu/Debian packaging places init in bash completion scripts
-source /etc/bash_completion.d/virtualenvwrapper
+export PIP_DOWNLOAD_CACHE=$HOME/.pip_download_cache
 
 alias python3='python3.3'
+
+export WORKON_HOME="/opt/virtualenvs"
+
+# My own simple virtualenvwrapper workalike
+workon() {
+    if [ -z "$1" ] ; then
+        find "$WORKON_HOME" -mindepth 1 -maxdepth 1 -type d -exec basename \{\} \;
+    else
+        . "$WORKON_HOME/$1/bin/activate"
+    fi
+}
+
+# With its own bash completion helper
+_virtualenv () {
+    local cur
+    # Pointer to current completion word.
+    # By convention, it's named "cur" but this isn't strictly necessary.
+    cur=${COMP_WORDS[COMP_CWORD]}
+
+    local candidates=$(find "$WORKON_HOME" -mindepth 1 -maxdepth 1 -type d -name "$cur*" -exec basename \{\} \;)
+    COMPREPLY=($(compgen -W "$candidates"))
+    return 0
+} && complete -F _virtualenv workon
