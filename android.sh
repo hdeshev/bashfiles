ea8e71cf16eaf3296581dcfd53c8a98e66b57013
Hristo Deshev <hristo@deshev.com>
2015-08-05 11:06:21 +0300

    android ndk

--- android.sh
+++ android.sh
@@ -1,9 +1,11 @@
 # Linux execute helpers
 alias ii='xdg-open'
 
+ANDROID_NDK=/opt/android-ndk
 ANDROID_SDK=/opt/android-sdk-linux
+export ANDROID_HOME=$ANDROID_SDK
 GENYMOTION_DIR=/data/vms/genymotion/genymotion
-export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$GENYMOTION_DIR
+export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$GENYMOTION_DIR:$ANDROID_NDK
 
 DART_EDITOR_DIR=/opt/dart
 export PATH=$PATH:$DART_EDITOR_DIR
